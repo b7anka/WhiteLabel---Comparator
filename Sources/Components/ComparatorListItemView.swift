e3f1254f5739035df218b98e56d00ade138b43ac
Joao Moreira <joao.moreira@go-evio.com>
2022-08-08 11:16:17 +0100

    Still implementing

--- Sources/Components/ComparatorListItemView.swift
+++ Sources/Components/ComparatorListItemView.swift
@@ -54,7 +54,7 @@ public struct ComparatorListItemView: View {
                         ZStack {
                             Image(uiImage: self.image)
                                 .resizable()
-                                .scaledToFit()()
+                                .scaledToFit()
                                 .frame(minWidth: 121, idealWidth: 165, maxWidth: 180, minHeight: 122, idealHeight: 122, maxHeight: 122)
                                 .onAppear {
                                     self.getImage()
