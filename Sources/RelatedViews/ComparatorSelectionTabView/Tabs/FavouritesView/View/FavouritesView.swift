81b1ff1b7e722662bacbb11c49eef5cdfd98c08b
Joao Moreira <joao.moreira@go-evio.com>
2022-08-12 15:55:02 +0100

    Still implementing

--- Sources/RelatedViews/ComparatorSelectionTabView/Tabs/FavouritesView/View/FavouritesView.swift
+++ Sources/RelatedViews/ComparatorSelectionTabView/Tabs/FavouritesView/View/FavouritesView.swift
@@ -21,7 +21,11 @@ public struct FavouritesView: View {
         ComparatorSelectionListView(chargers: self.$viewModel.chargers, selectedCharger: self.viewModel.chargerSelected)
             .onChange(of: self.viewModel.selectedCharger) { newValue in
                 guard let charger = newValue else { return }
-                self.comparatorChargers.insert(charger, at: self.comparatorChargers.count-1)
+                if !ComparatorItemModel.checkIfPlugOrChargerAlreadyExists(listOfChargers: self.comparatorChargers, chargerToAdd: charger) {
+                    self.comparatorChargers.insert(charger, at: self.comparatorChargers.count-1)
+                } else {
+                    EVIOLocalNotificationsManager.shared.showNotificationWithMessageAndTitle(self.viewModel.languageManager.comparatorPlugAlreadyBeingCompared, title: nil, style: .danger)
+                }
                 if self.comparatorChargers.count > .numberOfAllowedComparatorItems {
                     self.comparatorChargers.removeAll(where: { $0.isDefault })
                 }
