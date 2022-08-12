e4a2e37c7538d9877da7552ff68cc10eef0d8233
Joao Moreira <joao.moreira@go-evio.com>
2022-08-12 10:14:02 +0100

    Still implementing

--- Sources/RelatedViews/ComparatorSelectionTabView/RelatedViews/ChoosePlugView/View/ChoosePlugView.swift
+++ Sources/RelatedViews/ComparatorSelectionTabView/RelatedViews/ChoosePlugView/View/ChoosePlugView.swift
@@ -20,20 +20,19 @@ public struct ChoosePlugView: View {
         ZStack {
             Color.semiTransparent
                 .edgesIgnoringSafeArea(.all)
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.primaryBackground)
-                .overlay(
-                    VStack(spacing: 10) {
-                        Text("Choose the plug you want to compare")
-                            .modifier(EVIOAlertMessageModifier(color: .primaryTextColor, textAlignment: .center, lineLimit: nil))
-                        ChoosePlugListView(viewModel: self.viewModel)
-                        EVIOMainButton(disabled: self.$viewModel.okButtonDisabled, title: self.viewModel.languageManager.generalOk, action: self.viewModel.okButtonTapped)
-                            .padding(.horizontal, 45)
-                        Spacer()
-                    }
-                        .padding(10)
-                )
-                .padding(.horizontal, 15)
+            VStack(spacing: .zero) {
+                VStack(spacing: 10) {
+                    Text("Choose the plug you want to compare")
+                        .modifier(EVIOAlertMessageModifier(color: .primaryTextColor, textAlignment: .center, lineLimit: nil))
+                    ChoosePlugListView(viewModel: self.viewModel)
+                    EVIOMainButton(disabled: self.$viewModel.okButtonDisabled, title: self.viewModel.languageManager.generalOk, action: self.viewModel.okButtonTapped)
+                        .padding(.horizontal, 45)
+                }
+                    .padding(10)
+            }
+            .background(Color.primaryBackground)
+            .cornerRadius(20)
+            .padding(.horizontal, 15)
         }
         .background(EVIOTransparentBackgroundView())
     }
