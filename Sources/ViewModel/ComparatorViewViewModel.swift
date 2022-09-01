c8c5844b3a6ed87546619d26b05b82662dd0351c
Joao Moreira <joao.moreira@go-evio.com>
2022-09-01 17:28:02 +0100

    changed some files

--- Sources/ViewModel/ComparatorViewViewModel.swift
+++ Sources/ViewModel/ComparatorViewViewModel.swift
@@ -72,15 +72,15 @@ public final class ComparatorViewViewModel: ObservableObject {
         guard let selectedPlug = plug else { return }
         let timeTo: CGFloat = EVIOAppUtils.shared.getValueForSliderBarSize(plug: selectedPlug, ev: ev, maxPower: maxPower, maxValue: maxValue)
         time = timeTo / 60.0
-        guard let charger = item.charger, let estimateCost: EVIOEstimatedCost = EVIOEstimatedCostUtils.shared.calculateEstimatedCost(estimateTime: Double(timeTo / 60.0), contract: nil, charger: charger, plug: selectedPlug, ev: selectedEv) else {return}
-        item.totalCost = String(format: "%.\((charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? 0.0 : estimateCost.mainCost).numberOfDecimalPlaces(maxPlaces: 2))f€", locale: Locale.current, charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? 0.0 : estimateCost.mainCost) + " \(self.languageManager.generalPlusVat)"
+        guard let charger = item.charger, let estimateCost: EVIOEstimatedCost = EVIOEstimatedCostUtils.shared.calculateEstimatedCost(estimateTime: time, contract: nil, charger: charger, plug: selectedPlug, ev: selectedEv) else {return}
+        item.totalCost = String(format: "%.\((charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? .zero : estimateCost.mainCost).numberOfDecimalPlaces(maxPlaces: 2))f€", locale: Locale.current, charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? .zero : estimateCost.mainCost) + " \(self.languageManager.generalPlusVat)"
         item.totalPower = String(format: "%.\(estimateCost.consumption.numberOfDecimalPlaces(maxPlaces: 2))f %@", locale: Locale.current, estimateCost.consumption, self.languageManager.chargerDetailsKWh)
-        let pricePerKwh = (charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? 0.0 : estimateCost.mainCost) / estimateCost.consumption
-        item.averageCostPerKwh = String(format: "%.\(pricePerKwh.numberOfDecimalPlaces(maxPlaces: 4))f %@ %@", locale: Locale.current, pricePerKwh, "€/\(UOMS.kWh)", self.languageManager.generalPlusVat)
-        let pricePerMin = (charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? 0.0 : estimateCost.mainCost) / Double(timeTo / 60.0)
-        item.averageCostPerMinute = String(format: "%.\(pricePerMin.numberOfDecimalPlaces(maxPlaces: 4))f %@ %@", pricePerMin, "€/\(UOMS.min)", self.languageManager.generalPlusVat)
+        let pricePerKwh = (charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? .zero : estimateCost.mainCost) / estimateCost.consumption
+        item.averageCostPerKwh = String(format: "%.\(pricePerKwh.numberOfDecimalPlaces(maxPlaces: 4))f %@", locale: Locale.current, pricePerKwh, self.languageManager.generalPlusVat)
+        let pricePerMin = (charger.isMobie ? estimateCost.estimatedCostForMobie?.priceWithTaxesButNoVat ?? .zero : estimateCost.mainCost) / Double(time)
+        item.averageCostPerMinute = String(format: "%.\(pricePerMin.numberOfDecimalPlaces(maxPlaces: 4))f %@", pricePerMin, self.languageManager.generalPlusVat)
         self.chargingTime = Int(timeTo)
-        self.sliderDuration = String(format: "%@ %@", TimeInterval(timeTo.rounded(.up)).timeAsString(showDays: false, showSeconds: false), time >= 60 ? "h" : "min")
+        self.sliderDuration = String(format: "%@", TimeInterval(timeTo.rounded(.up)).timeAsString(showDays: false, showSeconds: false))
     }
     
     // MARK: - PUBLIC FUNCTIONS
